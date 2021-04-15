a3711bcd791ddefb20bb1c2960a9d283ae6b6447
zachmann00 <zach_mann@hotmail.com>
2021-04-15 10:26:57 -0400

    Finished assignment

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// 2b
+import Exercise from './exercise/Exercise'
+
+function App() {
+  return (
+    <div>
+    {/* 2c */}
+    <Exercise />
+  {/* 3a */}
+    <Exercise good='Awesome' bad="Terrible" />
+    </div>
+  );
+}
+
+export default App;
